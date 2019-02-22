3f9e40e46521181593dea67f287e83e718429d4a
Alex Bryner <abryner@doterra.com>
2019-02-22 02:56:52 -0700

    moved generator function to helper

--- force-app/main/default/aura/example/exampleHelper.js
+++ force-app/main/default/aura/example/exampleHelper.js
@@ -1,17 +1,38 @@
 ({
+    getGenerator: function (cmp, helper, promises, setTo) {
+        console.log('Hitting Generator : ' + setTo);
+        const generatorFunc = function* () {
+
+            let results = []
+            for (let i = 0; i < promises.length; i++) {
+                const res = yield promises[i];
+                // console.log('Promised Result : ' + res);
+                results.push(res);
+
+                // Would not suggest setting within the loop, for demo only
+                cmp.set(setTo, results);
+                console.log(setTo + ' Numbers : ' + cmp.get(setTo));
+            }
+
+            // Adding One Last Yield for Demo Purposes and Setting the Numbers Again
+            console.log('Running One More Ordered Promise : ' + 5);
+            results.push(yield helper.asyncPromiseWrapper(helper.dummyFunction, (500 * 5)));
+
+            cmp.set(setTo, results);
+            console.log(setTo + ' Numbers : ' + cmp.get(setTo));
+        }
+
+        return generatorFunc;
+    },
+
     // Calls and Resolves Promises Recursively, needed to manage the generator function
-    promiseChain: function (helper, iter, nextValue) {
-        console.log('Iterator - Next Value : ' + JSON.stringify(nextValue) + ' - ' + nextValue.value);
+    promiseChain: function (iter, nextValue) {
         if (!nextValue.done) {
             nextValue.value.then(result => {
-                helper.promiseChain(helper, iter, iter.next(result));
+                this.promiseChain(iter, iter.next(result));
             })
         }
     },
-
-
-
-    // Functions for demo purposes only
 
     // Returns a Promise that resolves after the SetTimeout
     asyncPromiseWrapper: function (someFunction, waitTime) {
@@ -28,21 +49,46 @@
         });
     },
 
-    getPromiseList: function () {
+
+    // Functions for demo purposes only
+
+    getPromiseList: function (sort) {
         let promises = [];
         for (let i = 1; i < 10; i++) {
-            const seconds = i * 1000;
-            console.log('Adding Promise with SetTimeout for : ' + seconds);
+            const seconds = i * 500;
             promises.push(this.asyncPromiseWrapper(this.dummyFunction, seconds));
         }
 
-        // Reversing the order for demonstrating the result yield
-        // showing the timeouts running out but waiting before setting results
-        promises.reverse();
+        if (sort === 'reverse') {
+            promises.reverse();
+        } else if (sort === 'shuffle') {
+            this.shuffle(promises);
+        }
+
         return promises;
     },
 
     dummyFunction: function (i) {
-        return 'New Number : ' + i;
+        return '-- ' + (i / 500) + ' --';
+    },
+
+    shuffle: function (array) {
+        var currentIndex = array.length,
+            temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
     }
 })
